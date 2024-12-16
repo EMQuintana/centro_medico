32b2d9c417f892f94c1b1c76194ec23e3bfa619d
EMQuintana <ernesto.quintana@inacapmail.cl>
2024-12-16 20:16:36 -0300

    dios soy yo de nuevo

--- centro_medico/settings.py
+++ centro_medico/settings.py
@@ -99,7 +99,9 @@ DATABASES = {
 }
 
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
 
 
 # Password validation
