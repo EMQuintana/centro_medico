b3f8044c6ef6908928b7bcbbf77747e77bcc22ec
EMQuintana <ernesto.quintana@inacapmail.cl>
2024-12-16 20:10:24 -0300

    dios ayuda

--- centro_medico/settings.py
+++ centro_medico/settings.py
@@ -12,12 +12,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -83,8 +88,13 @@ WSGI_APPLICATION = 'centro_medico.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ezNizefsdAhzwKcahiwXRLkKkztpjmTJ',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
 
