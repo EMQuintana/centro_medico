837ebcb161569b7841b1c55279265578240a084f
EMQuintana <ernesto.quintana@inacapmail.cl>
2024-12-16 21:01:24 -0300

    sin localhost

--- centro_medico/settings.py
+++ centro_medico/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = 'django-insecure-axly%(tw357c842r67aiby8%%2@j9fj7dj2^me-_to=(_z!5ku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
